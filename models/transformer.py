d96198e343a0aa3dc8432c549eed814f96c665b9
Victor Zhang <zxjzxj9@gmail.com>
2020-11-14 00:27:44 +0800

    Modify the model

--- models/transformer.py
+++ models/transformer.py
@@ -1,16 +1,44 @@
 #! /usr/bin/env python
 
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class TRPEncoder(nn.Module):
+class PosEmbedding(nn.Module):
+
+    def __init__(self, maxlen, ndim):
+        super().__init__()
+        self.embed = nn.Embedding(maxlen, ndim, padding_idx=0)
+
+    def forward(self, x):
+        return self.embed(x)
+
+class SinEmbedding(nn.Module):
     
-    def __init__(self, nfeat, nlayers, nff):
+    def __init__(self, maxlen, ndim):
         super().__init__()
 
+        pe = torch.zeros(maxlen, ndim)
+        pos = torch.arange(0, maxlen, dtype=torch.float).unsqueeze(1)
+        div = torch.exp(torch.arange(0, ndim, 2)).float()*(-math.log(10000.0)/ndim)
+        pe[:, 0::2] = torch.sin(pos*div)
+        pe[:, 1::2] = torch.cos(pos*div)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
 
-class TRPDeconder(nn.Module):
+    def forward(self, x):
+        return self.pe[:x.size(0), :]
 
-    def __init__(self, nfeat, nlayers, nff):
+
+class TRPModel(nn.Module):
+    
+    def __init__(self, nfeat, nlayers, nff, act_fn='relu'):
         super().__init__()
+
+    def forward(self, x):
+        pass
+
+
+if __name__ == "__main__":
+    pass
