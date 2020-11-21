a5439d90053ec71102b84dc5aa649acf20509756
Victor Zhang <zxjzxj9@gmail.com>
2020-11-21 20:13:39 +0800

    Add some parameter configs

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import os
 import sys
 import argparse
 
+parser = argparse.ArgumentParser("Transformer Reaction Predictor Argument Parser")
+parser.add_argument("-c", "--config-file", type="str", default="params.yaml", help="Config file path")
+
+arg = parser.parse_args()
+
 def train():
     pass
 
