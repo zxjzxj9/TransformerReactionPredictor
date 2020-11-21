a05008cfe9ab83c369c3aa5e2e7b99198b19b732
Victor Zhang <zxjzxj9@gmail.com>
2020-11-21 23:15:01 +0800

    Add some config file

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import argparse
+from .utils import Config, create_model_from_config
 
 parser = argparse.ArgumentParser("Transformer Reaction Predictor Argument Parser")
 parser.add_argument("-c", "--config-file", type="str", default="params.yaml", help="Config file path")
@@ -16,6 +17,9 @@ def predict():
     pass
 
 if __name__ == "__main__":
+    conf = Config(args.config_file)
+    model = create_model_from_config(conf)
+    
     if args.mode == "train":
         pass
     elif args.mode == "predict":
