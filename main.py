dbb19c3336566ab11b75d2226df3197f7e1a075e
Victor Zhang <zxjzxj9@gmail.com>
2020-12-07 23:33:53 +0800

    Add training code

--- main.py
+++ main.py
@@ -9,18 +9,26 @@ from .utils import Config, \
     create_optimizer_from_config
 import tqdm
 import torch.nn as nn
+import torch.nn.functional as F
+import apex
 
 parser = argparse.ArgumentParser("Transformer Reaction Predictor Argument Parser")
 parser.add_argument("-c", "--config-file", type="str", default="params.yaml", help="Config file path")
 parser.add_argument("-m", "--mode", type="str", default="train", help="Training mode")
 args = parser.parse_args()
 
-def train(model, optimizer, nepoch, train_data, valid_data, test_data):
-    ce_func = nn.CrossEntropyLoss()
+def train(model, optimizer, nepoch, train_data, valid_data, test_data, log_writer=None):
     for src, tgt in tqdm.tqdm(train_data):
         pred = model(src)
-        loss = ce(pred, tgt)
+        loss = F.binary_cross_entropy_with_logits(pred, tgt)
 
+        optimizer.zero_grad()
+        if hasattr(optimizer, "scale_loss"):
+            with optimizer.scale_loss(loss) as scaled_loss:
+                scaled_loss.backward()
+        else:
+            loss.backward()
+        optimizer.step()
 
 def predict():
     pass
